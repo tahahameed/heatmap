ccadfceee1a80432e6355350a722091e7df74a07
Taha Hameed <tahahameed1@gmail.com>
2024-10-19 17:15:15 +0500

    initial commit

---
+++ api.php
@@ -0,0 +1,42 @@
+[
+  { "x": 76, "y": 68 },
+  { "x": 412, "y": 267 },
+  { "x": 207, "y": 470 },
+  { "x": 850, "y": 375 },
+  { "x": 76, "y": 571 },
+  { "x": 949, "y": 491 }
+]
+
+
+<?php
+// $host = 'localhost';  // MySQL container service name
+// $user = 'root';
+// $pass = '';
+// $dbname = 'sis';
+
+// // Create connection
+// $conn = new mysqli($host, $user, $pass, $dbname);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// // echo "Connected successfully to MySQL!";
+
+// // If the connection is successful, run a query to check
+// $sql = "SELECT x, y FROM points";
+// $result = $conn->query($sql);
+
+// $points = [];
+// if ($result->num_rows > 0) {
+//     $points = $result->fetch_assoc();
+// } 
+
+// // Return the points as JSON
+// header('Content-Type: application/json');
+// echo json_encode($points);
+
+// Close the connection
+// $conn->close();
+
+?>
